e795b87bece391bc57c41586aed666b1f0ffcf61
Jessie <jessica.giraud@yahoo.fr>
2022-09-11 10:55:34 +0200

    Edit profile

---
+++ src/Form/UserType.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Form;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            
+            ->add('pseudo',TextType::class, [
+                'label' => 'Pseudo',
+            ])
+            
+            
+            ->add('image',FileType::class, ['required' => false, 'mapped' => false])
+           
+            ->add('presentation',TextareaType::class, [
+                'required' => true,
+                'label' => 'Votre message*',
+                'constraints' => new Length(
+                    [
+                        'min' => 8,
+                        'max' => 200, //limit the number of caracters
+                    ]
+                ),
+                
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Envoyer',
+                'attr' => [
+                    'class' => 'btn',
+            
+                ]
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
